16db385de6a8d1addfa51efa48976482303b0174
Nadir Hamza <hamzanadir.freelance@gmail.com>
2021-11-16 11:40:39 +0100

    =v1

---
+++ script.js
@@ -0,0 +1,21 @@
+function get(id){
+    return document.getElementById(id)
+}
+
+var titre1 = get('titre1')
+//traitement 
+titre1.onclick=clickTitre1;
+
+function clickTitre1(){
+    alert('hi')
+}
+var titre2 = get('titre2')
+//traitement 
+var titre3 = get('titre3')
+//traitement 
+var para1 = get('para1')
+//traitement 
+var para2 = get('para2')
+//traitement 
+var para3 = get('para3')
+//traitement 
